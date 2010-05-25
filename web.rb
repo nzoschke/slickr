7672e3891e6212f8069fa3ddee4e1aad7d65618b
Noah Zoschke <noah@heroku.com>
2010-05-25 09:28:33 -0700

    use :set => http://flickr.com... in sitemap; set to feed helper

--- web.rb
+++ web.rb
@@ -6,4 +6,11 @@ get '/' do
 end
 
 helpers do
+  def feed(set_url)
+    # http://www.flickr.com/photos/7243296@N02/sets/72157623199241973/
+    # to
+    # http://api.flickr.com/services/feeds/photoset.gne?set=72157623199241973&nsid=7243296@N02&lang=en-us&format=json
+    parts = set_url.split '/'
+    return "http://api.flickr.com/services/feeds/photoset.gne?set=#{parts[6]}&nsid=#{parts[4]}&lang=en-us&format=json"
+  end
 end
