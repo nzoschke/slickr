3c41fd7c0d36c1c31002cf1ba5d304d7ab0876bf
Noah Zoschke <noah@heroku.com>
2010-05-25 09:49:32 -0700

    integrage javascript in web

--- web.rb
+++ web.rb
@@ -7,10 +7,9 @@ end
 
 helpers do
   def feed(set_url)
-    # http://www.flickr.com/photos/7243296@N02/sets/72157623199241973/
-    # to
-    # http://api.flickr.com/services/feeds/photoset.gne?set=72157623199241973&nsid=7243296@N02&lang=en-us&format=json
+    # http://www.flickr.com/photos/7243296@N02/sets/72157623199241973/ =>
+    # http://api.flickr.com/services/feeds/photoset.gne?set=72157623199241973&nsid=7243296@N02&lang=en-us&format=json&jsoncallback=?
     parts = set_url.split '/'
-    return "http://api.flickr.com/services/feeds/photoset.gne?set=#{parts[6]}&nsid=#{parts[4]}&lang=en-us&format=json"
+    return "http://api.flickr.com/services/feeds/photoset.gne?set=#{parts[6]}&nsid=#{parts[4]}&lang=en-us&format=json&jsoncallback=?"
   end
 end
