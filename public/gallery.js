9bb3a64f5ab2967710bfdf1192b17773a4b777f2
Noah Zoschke <noah@heroku.com>
2010-05-26 08:04:48 -0700

    thumbnail workflow works

--- public/gallery.js
+++ public/gallery.js
@@ -7,7 +7,7 @@ $('a[feed]').live('click', function(e) {
     var thumbs = "<ul>";
     $.each(data.items, function(i,item) {
       var thumb_url = (item.media.m).replace("_m.jpg", "_s.jpg"); // 75x75 square
-      thumbs += '<li><img class="thumb" src="' + thumb_url + '" alt="' + item.title + '" title="' + item.title + '"/></li>';
+      thumbs += '<li><img class="thumb" src="/thumb?' + 't=' + new Date().getTime() + '&url=' + thumb_url + '" alt="' + item.title + '" title="' + item.title + '"/></li>';
     });
     thumbs += "</ul>";
 
@@ -30,18 +30,33 @@ $('img.thumb').live('click', function(e) {
 // admin
 
 $('#photo').live('mousedown', function(e) {
-  if ($("#resizable")) $("#resizable").remove();
+  $('#close').trigger('click');
+  $('#photowrap').append('<div id="resizable" class="ui-widget-content"><a id="save" href="#">save</a><a id="close" href="#" style="margin-left: 2px; float: left;">x</a></div>');
+  $("#resizable").resizable({ aspectRatio: 1/1, minWidth: 75, minHeight: 75, containment: 'parent' });
+  $("#resizable").draggable({ containment: '#jpg', cursor: 'move' });
+  $("#resizable").css({ position: 'absolute', left: e.layerX - 75, top: e.layerY - 75, width: 150, height: 150, opacity: 0.65, cursor: 'move', border: '1px solid #0080FF' });
+});
 
-  $('#photowrap').append('<div id="resizable" class="ui-widget-content"><h3 class="ui-widget-header">Resizable</h3></div>');
+$('#save').live('click', function(e) {
+  $("#save").remove(); // prevent clicking again
+  $("#close").html('');
+  $("#resizable").append('<i>saving...</i>');
+  e.preventDefault();
   
-  $("#resizable").resizable({
-    aspectRatio: 1/1,
-    containment: 'parent',
+  $.post("/thumb", {
+    photo_url: $("#jpg").attr('src'),
+    x: $("#resizable").attr('offsetLeft') - $("#jpg").attr('offsetLeft'),
+    y: $("#resizable").attr('offsetTop')  - $("#jpg").attr('offsetTop'),
+    width: $("#resizable").attr('clientWidth')
+  }, function (e) {
+    $('img.thumb.selected').attr('src', $('img.thumb.selected').attr('src') + '&t=' + new Date().getTime()); // trigger reload
+    $('#close').trigger('click');
   });
-  
-  $("#resizable").draggable({
-    containment: 'parent',
-  });
-  
-  $("#resizable").css({ position: 'absolute', left: e.layerX, top: e.layerY, width: 100, height: 100 });
+});
+
+$('#close').live('click', function(e) {
+  if (!$("#resizable")) return;
+  $("#resizable").resizable('destroy').draggable('destroy');  
+  $("#resizable").remove();
+  e.preventDefault()
 });
